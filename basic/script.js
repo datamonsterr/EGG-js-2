23dfe980b7c3d709487ae144d2e682825742ac63
datamonsterr <phamthanhdat17092004@gmail.com>
2024-10-05 16:59:21 +0700

    add event

---
+++ basic/script.js
@@ -0,0 +1,111 @@
+// // Strings
+// let string = "       Hello World           ";
+// console.log(string.length);
+// string = string.trim();
+// console.log(string.length);
+// let arr = string.split(" ");
+// console.log(arr);
+// console.log(arr.join(" the "));
+
+// String.prototype.addDat = function () {
+//     return this + " Dat";
+// }
+// let str = "Hello";
+// str = str.addDat();
+// console.log(str);
+
+// // Date time
+// now = new Date();
+// console.log(`Today is ${now}`);
+// birthday = new Date(2004, 9, 17);
+// console.log(`My birthday is ${birthday}`);
+// console.log(`My age is ${now.getFullYear() - birthday.getFullYear()}`);
+
+// // Iterables
+// // let arr2 = [1, 2, 3, 4, 5];
+// // let iter = arr2[Symbol.iterator]();
+// // console.log(iter.next());
+// let myObj = {
+//     1: "a",
+//     2: "b",
+//     3: "c",
+//     [Symbol.iterator]: function () {
+//         let i = 1;
+//         return {
+//             next: () => {
+//                 return {
+//                     value: `Turn ${i}: ${this[i]}`,
+//                     done: i++ > 3
+//                 }
+//             }
+//         }
+//     }
+// }
+// let iter = myObj[Symbol.iterator]();
+// for (let i of myObj) {
+//     console.log(i);
+// }
+
+// // Destructuring
+// let arr3 = [1, 2, 3];
+// let [a, b, c] = arr3;
+// console.log(a, b, c);
+// let obj = {
+//     name: "Dat",
+//     age: 17
+// }
+// let { name, age } = obj;
+// console.log(name, age);
+
+// function foo(...args) {
+//     let [a, b, c, ...rest] = args;
+//     console.log(`a, b, c = ${a}, ${b}, ${c}`);
+//     for (let i of rest) {
+//         console.log(`i = ${i}`);
+//     }
+// }
+// console.log(foo(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+// // Ex: Turn object into HTML
+// let myHTML = {
+//     tag: "div",
+//     children: [
+//         {
+//             tag: "div",
+//             children: [
+//                 {
+//                     tag: "button",
+//                     text: "click me"
+//                 }
+//             ],
+//             text: "Hello World"
+//         },
+//         {
+//             tag: "p",
+//             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+//         }
+//     ]
+// }
+
+// const reveal = () => {
+//     let htmlString = "";
+//     htmlString += `<${myHTML.tag}>`;
+//     myHTML.children.forEach((child) => {
+//         htmlString += `<${child.tag}>`;
+//         if (child.text) {
+//             htmlString += child.text;
+//         }
+//         if (child.children) {
+//             child.children.forEach((child) => {
+//                 htmlString += `<${child.tag}>`;
+//                 if (child.text) {
+//                     htmlString += child.text;
+//                 }
+//                 htmlString += `</${child.tag}>`;
+//             })
+//         }
+//         htmlString += `</${child.tag}>`;
+//     })
+//     htmlString += `</${myHTML.tag}>`;
+//     document.body.innerHTML = htmlString;
+// }
